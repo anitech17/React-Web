99aad624b705e5a243f006cc5156ebdd89662cff
Animesh Pandey <animeshpandey1726@gmail.com>
2025-08-06 19:44:46 +0530

    fetch students API integration in educators portal

--- src/Pages/EducatorPages/EducatorStudents.tsx
+++ src/Pages/EducatorPages/EducatorStudents.tsx
@@ -1,19 +1,46 @@
 // pages/index.tsx
-import { Box } from "@mui/material";
-import { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../app/store";
 import { StudentDialog, StudentTable } from "./Components";
+import { Spinner } from "../../Components";
+import { fetchEducatorStudents } from "../../features/educator/thunks";
 
 export const EducatorStudents = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  const { data: students, loading, error } = useSelector(
+    (state: RootState) => state.educator.educatorStudents
+  );
+
   const [selectedStudent, setSelectedStudent] = useState<any>(null);
+
+  useEffect(() => {
+    if (user?.id) {
+      dispatch(fetchEducatorStudents(user.id));
+    }
+  }, [dispatch, user?.id]);
 
   return (
     <Box width="100%" sx={{ p: 3 }}>
-      <StudentTable onSelect={setSelectedStudent} />
-      <StudentDialog
-        open={!!selectedStudent}
-        onClose={() => setSelectedStudent(null)}
-        student={selectedStudent}
-      />
+      {loading && <Spinner />}
+      {error && (
+        <Typography variant="body2" color="error">
+          {error}
+        </Typography>
+      )}
+      {!loading && students && (
+        <>
+          <StudentTable data={students} onSelect={setSelectedStudent} />
+          <StudentDialog
+            open={!!selectedStudent}
+            onClose={() => setSelectedStudent(null)}
+            student={selectedStudent}
+          />
+        </>
+      )}
     </Box>
   );
 };
